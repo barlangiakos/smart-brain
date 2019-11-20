a91a98fdd4a1c4072c89a44a1972f9df1fbd4b8f
barlangiakos <barlangi@hotmail.com>
2019-11-20 09:52:27 +0100

    changing response

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-	res.send(database.users);
+	res.send('it is working');
 })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) } )
@@ -38,5 +38,5 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) } )
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) } )
 
 app.listen(process.env.PORT || 3000, () => {
-	console.log('app is running on port 3000');
+	console.log(`app is running on port ${process.env.PORT}`);
 });
