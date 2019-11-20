3335fa98dfe0a0246df9a6852e096e400c389096
barlangiakos <barlangi@hotmail.com>
2019-11-20 12:26:00 +0100

    trying to fix cors

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const db = knex({
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({credentials: true, origin: true}));
 
 app.get('/', (req, res) => {
 	res.send('it is working');
