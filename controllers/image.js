de959f95ab5b81b8b742e9d6725b18f648a08452
barlangiakos <barlangi@hotmail.com>
2019-11-20 18:25:56 +0100

    fixing returning

--- controllers/image.js
+++ controllers/image.js
@@ -11,7 +11,7 @@ const handleImage = (req, res, db) => {
 	const {id} = req.body;
 	db('users').where('id', '=', id)
 	  .increment('entries', 1)
-	  .returning()
+	  .returning('entries')
 	  .then(entries => {
 	  	res.json(entries);
 	  })
